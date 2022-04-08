ef304e75d0b9da3893e53055dcc34b1e2848b574
tomek <tomaszpawelkowalski@gmail.com>
2022-04-08 09:49:57 +0200

    Added showTask to class taskDto

--- src/dto/taskDto.ts
+++ src/dto/taskDto.ts
@@ -3,4 +3,14 @@ export class Task {
   title: string;
   description: string;
   status: false;
+  constructor(id: number, title: string, description: string, status: false) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.status = status;
+  }
+
+  showTask() {
+    console.log(`${this.id} ${this.title} ${this.description} ${this.status}`);
+  }
 }
