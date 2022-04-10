0460e60f79c28092c2253c3da5513ae6d9f0a6a6
tomek <tomaszpawelkowalski@gmail.com>
2022-04-10 13:31:52 +0200

    reorganized/clean up files

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -3,84 +3,92 @@ import { ToDoListService } from './app.service';
 import { NewTaskDto } from './dto/newTaskDto';
 
 describe(ToDoListService.name, () => {
-  it('Add task to the list', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    expect(List.toDoList.length).toBe(1);
+  describe('Adding task to the list', () => {
+    it('Add task to the list', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      expect(List.toDoList.length).toBe(1);
+    });
   });
-
-  it('Should show  specific task from the list', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    expect(List.showTask(0)).toHaveProperty('title', task.title);
+  describe('Show specific task', () => {
+    it('Should show  specific task from the list', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      expect(List.showTask(0)).toHaveProperty('title', task.title);
+    });
   });
+  describe('Toggle status', () => {
+    it('Should toggle task status', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.addTask(task);
+      List.changeStatus(0);
+      expect(List.toDoList[1].status).toBe(false);
+    });
 
-  it('Should toggle task status', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.addTask(task);
-    List.changeStatus(0);
-    expect(List.toDoList[1].status).toBe(false);
+    it('Should toggle task status', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.addTask(task);
+      List.changeStatus(0);
+      List.changeStatus(0);
+      expect(List.toDoList[0].status).toBe(false);
+      expect(List.toDoList[1].status).toBe(false);
+    });
   });
-  it('Should toggle task status', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.addTask(task);
-    List.changeStatus(0);
-    List.changeStatus(0);
-    expect(List.toDoList[0].status).toBe(false);
-    expect(List.toDoList[1].status).toBe(false);
+  describe('Updated task', () => {
+    it('Should change task', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      const newTask = new Task(2, 'NewTitle', 'description', false, new Date());
+      List.changeTask(0, newTask);
+      expect(List.toDoList[0].title).toBe('NewTitle');
+    });
   });
+  describe('Removing task from lists', () => {
+    it('Should remove task from list ', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.removeTask(0);
+      expect(List.toDoList.length).toBe(0);
+    });
 
-  it('Should change task', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    const newTask = new Task(2, 'NewTitle', 'description', false, new Date());
-    List.changeTask(0, newTask);
-    expect(List.toDoList[0].title).toBe('NewTitle');
+    it('Should remove task from list ', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.addTask(task);
+      List.addTask(task);
+      List.removeTask(1);
+      expect(List.toDoList.length).toBe(2);
+    });
   });
+  describe('Filter task', () => {
+    it('Should show only done tasks ', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.addTask(task);
+      List.addTask(task);
+      List.changeStatus(1);
+      expect(List.showTasksWithStatus(true).length).toBe(1);
+    });
 
-  it('Should remove task from list ', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.removeTask(0);
-    expect(List.toDoList.length).toBe(0);
-  });
+    it('Should show only undone tasks ', () => {
+      const task = new NewTaskDto('title', 'description', false);
+      const List = new ToDoListService();
+      List.addTask(task);
+      List.addTask(task);
+      List.addTask(task);
+      List.changeStatus(2);
 
-  it('Should remove task from list ', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.addTask(task);
-    List.addTask(task);
-    List.removeTask(1);
-    expect(List.toDoList.length).toBe(2);
-  });
-
-  it('Should show only done tasks ', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.addTask(task);
-    List.addTask(task);
-    List.changeStatus(1);
-    expect(List.showTasksWithStatus(true).length).toBe(1);
-  });
-
-  it('Should show only undone tasks ', () => {
-    const task = new NewTaskDto('title', 'description', false);
-    const List = new ToDoListService();
-    List.addTask(task);
-    List.addTask(task);
-    List.addTask(task);
-    List.changeStatus(2);
-
-    expect(List.showTasksWithStatus(true).length).toBe(1);
+      expect(List.showTasksWithStatus(true).length).toBe(1);
+    });
   });
 });
