e254932672f557dd181bda4a45e664064222d37e
tomek <tomaszpawelkowalski@gmail.com>
2022-04-10 18:20:55 +0200

    Add swagger response.

--- src/ToDoList.ts
+++ src/ToDoList.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsArray, ValidateNested } from 'class-validator';
 import { Task } from './dto/taskDto';
@@ -6,6 +7,7 @@ export class ToDoList {
   @ValidateNested()
   @Type(() => Task)
   @IsArray()
+  @ApiProperty({ type: () => Array(Task) })
   toDoList: Task[];
   constructor() {
     this.toDoList = [];
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,16 +9,22 @@ import {
   Query,
   ValidationPipe,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ToDoListService } from './app.service';
 import { NewTaskDto } from './dto/newTaskDto';
 import { QueryDto } from './dto/queryDto';
 import { Task } from './dto/taskDto';
+import { ToDoList } from './ToDoList';
 @ApiTags('ToDo List')
 @Controller('todolist')
 export class AppController {
   constructor(private readonly toDoService: ToDoListService) {}
   @Get()
+  @ApiResponse({
+    description: 'Successful operation',
+    status: 200,
+    type: ToDoList,
+  })
   getData(@Query() queryDto: QueryDto) {
     if (Object.keys(queryDto).length === 0) {
       return this.toDoService.showAllTasks();
@@ -27,21 +33,41 @@ export class AppController {
     }
   }
   @Post('add')
+  @ApiResponse({
+    description: 'Successful operation',
+    status: 201,
+    type: ToDoList,
+  })
   addTask(@Body(new ValidationPipe({ transform: true })) taskDto: NewTaskDto) {
     this.toDoService.addTask(taskDto);
     return this.toDoService.showAllTasks();
   }
   @Put('updateStatus/:index')
+  @ApiResponse({
+    description: 'Successful operation',
+    status: 200,
+    type: ToDoList,
+  })
   updateStatus(@Param('index') index: string) {
     this.toDoService.changeStatus(Number(index));
     return this.toDoService.showAllTasks();
   }
   @Put('updateTask/:index')
+  @ApiResponse({
+    description: 'Successful operation',
+    status: 200,
+    type: ToDoList,
+  })
   updateTask(@Param('index') index: string, @Body() taskDto: Task) {
     this.toDoService.changeTask(Number(index), taskDto);
     return this.toDoService.showAllTasks();
   }
   @Delete('remove/:index')
+  @ApiResponse({
+    description: 'Successful operation',
+    status: 200,
+    type: ToDoList,
+  })
   removeTask(@Param('index') index: string) {
     this.toDoService.removeTask(Number(index));
     return this.toDoService.showAllTasks();
