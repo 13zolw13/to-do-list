332ceb8e9f89df1b8228344bcbeb116f377b81c3
tomek <tomaszpawelkowalski@gmail.com>
2022-04-11 18:47:58 +0200

    set up sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { ToDoListService } from './service/app.service';
 import configuration from '../config/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [ToDoListService],
 })
