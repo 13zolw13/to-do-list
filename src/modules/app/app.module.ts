d54710eea9f2465f6d0daf777f951d2c65e76aab
tomek <tomaszpawelkowalski@gmail.com>
2022-04-29 22:38:14 +0200

    fix: remove connection to db.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,20 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from '../../../config/config';
 import { AppController } from './app.controller';
 import { ToDoListService } from './service/app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ load: [configuration] }),
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
+  imports: [ConfigModule.forRoot({ load: [configuration] })],
   controllers: [AppController],
   providers: [ToDoListService],
 })
